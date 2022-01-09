0cdc3192e6288a75e1095c4cea96d90bebef7b00
James <teejaytiger@gmail.com>
2022-01-08 22:33:58 -0800

    20 or less arbitrarily

--- WorstMovieRecommender.py
+++ WorstMovieRecommender.py
@@ -1,28 +1,20 @@
 import random
 import webbrowser
-import urllib.request
 from urllib.request import build_opener, HTTPCookieProcessor
 
 class gentt:
 	def __init__(self):
-		self.url=""
-		self.val = self.gen_val()
-		self.fail='https://www.imdb.com/title/tt5865147/'
-		
+		self.url=self.gen_val()	
 	def gen_val(self):
-		self.val = "tt"+''.join(["{}".format(random.randint(0, 9)) for num in range(0, 7)])
-		self.url="https://www.imdb.com/title/"+self.val+"/"
+		val = "tt"+''.join(["{}".format(random.randint(0, 9)) for num in range(0, 7)])
+		self.url="https://www.imdb.com/title/"+val+"/"
 
 def pick_movie(c):
-	opener = build_opener(HTTPCookieProcessor())
 	try:
-		response = opener.open(c.url)
-		print(c.url + " success")
+		response = build_opener(HTTPCookieProcessor()).open(c.url)
 	except:
-		print(c.url + " fail")
 		c.gen_val()
 		pick_movie(c)
 		
-c = gentt()
-pick_movie(c)
+pick_movie(gentt())
 webbrowser.open(c.url)
